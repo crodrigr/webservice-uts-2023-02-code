759c9d0eba4038904d2234464dab879f7ae651a7
Camilo Rodriguez <crodrigr@gmail.com>
2023-08-19 17:07:02 -0500

    actulizar

--- src/main/java/co/edu/uts/firstapp/controllers/IndexController.java
+++ src/main/java/co/edu/uts/firstapp/controllers/IndexController.java
@@ -3,6 +3,7 @@ package co.edu.uts.firstapp.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -12,6 +13,19 @@ public class IndexController {
     @GetMapping("/saludo")
     public String saludo(){
         return "Hola, cómo estas:CAMILO";
+    }
+
+    @GetMapping("/sqrs")
+    public String sqrs(@RequestParam double numero){
+        try{
+           return Double.toString(Math.sqrt(numero));
+        }catch(NumberFormatException e){
+            return "numero no valido";            
+        }
+        catch(Exception e){
+            return "numero no valido";            
+        }
+        
     }
     
 }
